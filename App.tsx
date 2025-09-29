4e2a58cae587550d2014fdeaf3bf9e9ea3251007
Marc <m.sarroca@gmail.com>
2025-09-29 19:28:37 +0200

    feat: format file

--- App.tsx
+++ App.tsx
@@ -1,16 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -28,10 +18,7 @@ function AppContent() {
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <NewAppScreen templateFileName="App.tsx" safeAreaInsets={safeAreaInsets} />
     </View>
   );
 }
