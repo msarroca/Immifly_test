1135739b2750aec41a27daca79382f26ef775cf7
Marc <m.sarroca@gmail.com>
2025-09-30 14:36:29 +0200

    feat: add prettier as tsconfig

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "singleQuote": true,
-  "trailingComma": "all",
-  "bracketSpacing": true,
-  "semi": true,
-  "printWidth": 100
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  semi: true,
+  printWidth: 100,
 };
