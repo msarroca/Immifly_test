d9f5b5ebf29d44da92e90101508cab9e966bfa9f
Marc <m.sarroca@gmail.com>
2025-09-30 14:36:50 +0200

    feat: add babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@app': './src/app',
+          '@features': './src/features',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@lib': './src/lib',
+        },
+      },
+    ],
+  ],
 };
