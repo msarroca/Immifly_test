45a285c558299bf89602503c8d5e3fbdfa8802c6
Marc <m.sarroca@gmail.com>
2025-10-01 15:06:58 +0200

    feat: add models and helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,9 @@
-import type { Product } from '@features/products/products.slice';
-
-export type CartItem = { product: Product; quantity: number };
+import { Currency, CartItem } from '@models/index';
+import { RATES } from '@constants/sales';
 
 export const calculateTotalEUR = (items: CartItem[]): number => {
   return items.reduce((sum, i) => sum + i.product.price * i.quantity, 0);
 };
+
+export const convertRates = (valueEUR: number, currency: Currency) =>
+  new Intl.NumberFormat('en', { style: 'currency', currency }).format(valueEUR * RATES[currency]);
---
+++ src/models/index.ts
@@ -0,0 +1,39 @@
+import { CURRENCIES, SALE_TYPES } from '@constants/sales';
+export type RootStackParamList = {
+  Products: undefined;
+  Ticket: undefined;
+};
+
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+};
+
+export type CartItem = { product: Product; quantity: number };
+
+export type CartState = { items: CartItem[] };
+
+export type ProductsState = {
+  items: Product[];
+  status: 'initial' | 'loading' | 'succeeded' | 'failed';
+  error?: string;
+};
+
+export type SalesState = {
+  currency: Currency;
+  saleType: SaleType;
+  status: 'initial' | 'loading' | 'succeeded' | 'failed';
+  error?: string;
+  lastMessage?: string;
+};
+
+export type Currency = (typeof CURRENCIES)[number]['key'];
+export type SaleType = (typeof SALE_TYPES)[number]['key'];
+
+export type PaymentError = {
+  message: string;
+};
